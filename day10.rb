1544ac57d289eec7e5c4316513c5feaec80e83da
Eric Rykwalder <e.rykwalder@gmail.com>
2020-12-10 22:30:16 -0500

    add day 10 solution

---
+++ day10.rb
@@ -0,0 +1,32 @@
+def read_volts
+  File.open('input10.txt').each.reject(&:empty?).map(&:to_i)
+end
+
+def day10a
+  last_volt = 0
+  diffs = read_volts.sort.map do |volt|
+    diff = volt - last_volt
+    last_volt = volt
+    diff
+  end.reduce(Hash.new(0)) {|h, v| h[v] += 1; h}
+  diffs[3] += 1
+  diffs[1] * diffs[3]
+end
+
+def day10b
+  volts = read_volts.push(0).sort
+  volts << volts[-1] + 3
+  memos = {}
+  count_paths = lambda do |i|
+    memos[i] ||= [
+      (i+1..i+3)
+        .reject {|j| j >= volts.size || volts[j] > volts[i] + 3}
+        .map {|j| count_paths.call(j)}
+        .sum,
+      1
+    ].max
+  end
+  count_paths.call(0)
+end
+
+p day10b
